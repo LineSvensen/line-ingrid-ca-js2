7e49edfedfde04ea09046981f188ad9e1db0a9c0
Ingrid Ornum <ingrid.ornum@gmail.com>
2024-10-02 15:30:44 +0200

    Feat: Added delete post function

--- src/js/router/views/postEdit.js
+++ src/js/router/views/postEdit.js
@@ -1,5 +1,6 @@
 import { authGuard } from "../../utilities/authGuard";
 import { onUpdatePost, getPostData } from "../../ui/post/update.js";
+import { deletePost } from "../../api/post/update.js";
 
 authGuard();
 
@@ -7,13 +8,37 @@ const form = document.getElementById('edit-posts-form');
 
     if (form) {
 
-        console.log('Form found, adding event listener for submit');
-
         form.addEventListener('submit', (event) => {
-            console.log('Submit event triggered');
-
             onUpdatePost(event);
         });
+
+        const deleteBtn = document.getElementById('delete-btn');
+        if (deleteBtn) {
+            deleteBtn.addEventListener('click', async () => {
+                const postId = localStorage.getItem('editPostId');
+                if (!postId) {
+                    console.error('No postId found in local storage.');
+                    return;
+                }
+
+                const confirmDelete = confirm('Are you sure you want to delete this post?');
+                if (confirmDelete) {
+                    try {
+                        const response = await deletePost(postId);
+                        if (response.ok) {
+                            alert('Post deleted successfully!');
+                            window.location.href = '/profile/';
+                        } else {
+                            alert('Failed to delete the post.');
+                        }
+                    } catch (error) {
+                        console.error('Error deleting post:', error);
+                        alert('An error occurred while deleting the post.');
+                    }
+                }
+
+            })
+        }
 
         const postId = localStorage.getItem('editPostId');
 
