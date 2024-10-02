ae3fc8bfedd84823d9faedf1e770b84002b3d6f6
Line Svensen <144799112+LineSvensen@users.noreply.github.com>
2024-10-02 18:19:20 +0200

    some fixes

--- src/js/router/views/postEdit.js
+++ src/js/router/views/postEdit.js
@@ -1,52 +1,38 @@
 import { authGuard } from "../../utilities/authGuard";
 import { onUpdatePost, getPostData } from "../../ui/post/update.js";
-import { onDeletePost } from "../../ui/post/delete.js";
 
 authGuard();
 
+/**
+ * Event listener for form submission.
+ *
+ * @param {Event} event - The event object triggered by form submission.
+ */
+
+/**
+ * Fetches post data if postId is found in local storage.
+ * @param {string|null} postId - The ID of the post to be edited, retrieved from local storage.
+ */
+
 const form = document.getElementById('edit-posts-form');
-const deleteBtn = document.getElementById('delete-btn');
 
-if (form) {
-    form.addEventListener('submit', async (event) => {
-        await onUpdatePost(event);
-    });
+    if (form) {
 
-    const postId = localStorage.getItem('editPostId');
-    if (postId) {
-        getPostData(postId);
-    } else {
-        console.error('No postId found in local storage.');
-    }
+        console.log('Form found, adding event listener for submit');
+
+        form.addEventListener('submit', (event) => {
+            console.log('Submit event triggered');
+
+            onUpdatePost(event);
+        });
+
+        const postId = localStorage.getItem('editPostId');
+
+        if (postId) {
+            getPostData(postId);
+        } else {
+            console.error('No postId found in local storage.');
+        }
 } else {
-    console.error('Form not found.');
+    console.error('Form not found.')
 }
-
-
-if (form) {
-    /**
-     * Handles the form submission for updating a post.
-     *
-     * @param {Event} event - The event object representing the form submission.
-     */
-    form.addEventListener('submit', (event) => {
-        console.log('Submit event triggered');
-
-        onUpdatePost(event);
-    });
-
-    const postId = localStorage.getItem('editPostId');
-
-    if (postId) {
-        getPostData(postId);
-    } else {
-        console.error('No postId found in local storage.');
-    }
-
-    if (deleteBtn) {
-        deleteBtn.addEventListener('click', onDeletePost);
-
-    } else {
-        console.error('Delete button not found.');
-    }
-};
